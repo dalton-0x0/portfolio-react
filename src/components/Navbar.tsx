bc06f93e23128ce41a0118a384d67a1b021b0366
Dalton Okechukwu <dalton.0x0@gmail.com>
2025-02-17 12:16:27 -0700

    commit: updates nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,125 @@
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+import { Menu as MenuIcon, Close as CloseIcon } from "@mui/icons-material";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menuItems = ["home", "about", "projects", "contact"];
+
   return (
-    <AppBar position="static">
+    <AppBar
+      position="static"
+      sx={{ background: "transparent", boxShadow: "none", color: "inherit" }}
+    >
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography
+          variant="h6"
+          component={RouterLink}
+          to="/"
+          sx={{
+            flexGrow: 1,
+            textDecoration: "none",
+          }}
+        >
           Portfolio
         </Typography>
-        <Box>
-          <Button color="inherit" component={RouterLink} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={RouterLink} to="/about">
-            About
-          </Button>
-          <Button color="inherit" component={RouterLink} to="/projects">
-            Projects
-          </Button>
-          <Button color="inherit" component={RouterLink} to="/contact">
-            Contact
-          </Button>
+
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          {menuItems.map((item) => (
+            <Box
+              key={`link-${item}`}
+              sx={{
+                position: "relative",
+                mx: 1,
+                "&::before": {
+                  content: '""',
+                  position: "absolute",
+                  bottom: -2,
+                  left: 0,
+                  width: "100%",
+                  height: 2,
+                  bgcolor: "primary.main",
+                  transform: "scaleX(0)",
+                  transition: "transform 0.3s ease",
+                },
+                "&:hover::before": {
+                  transform: "scaleX(1)",
+                },
+              }}
+            >
+              <Typography
+                component={RouterLink}
+                to={item === "home" ? "/" : `/${item}`}
+                sx={{
+                  color: "inherit",
+                  textDecoration: "none",
+                  textTransform: "capitalize",
+                }}
+              >
+                {item}
+              </Typography>
+            </Box>
+          ))}
         </Box>
+
+        <IconButton
+          sx={{ display: { xs: "flex", md: "none" } }}
+          onClick={() => setIsOpen(true)}
+          color="inherit"
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Drawer
+          anchor="right"
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          PaperProps={{
+            component: motion.div,
+            initial: { x: 300 },
+            animate: { x: 0 },
+            transition: { duration: 0.85, ease: "easeOut" },
+            sx: { width: 250 },
+          }}
+        >
+          <Box sx={{ p: 2 }}>
+            <IconButton onClick={() => setIsOpen(false)} sx={{ mb: 2 }}>
+              <CloseIcon />
+            </IconButton>
+            <List>
+              {menuItems.map((item) => (
+                <ListItem
+                  key={item}
+                  component={RouterLink}
+                  to={item === "home" ? "/" : `/${item}`}
+                  onClick={() => setIsOpen(false)}
+                  sx={{
+                    color: "text.primary",
+                    textDecoration: "none",
+                  }}
+                >
+                  <ListItemText
+                    primary={item}
+                    sx={{ textTransform: "capitalize" }}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
       </Toolbar>
     </AppBar>
   );
